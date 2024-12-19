d5b5baf10f144a70321ad2c670688c7533d25aa8
PolarStarWalker <savmot1929@gmail.com>
2024-12-19 17:42:19 +0300

    third

--- 03_finite_state_machines/03_01_detect_sequence_using_fsm/03_01_detect_sequence_using_fsm.sv
+++ 03_finite_state_machines/03_01_detect_sequence_using_fsm/03_01_detect_sequence_using_fsm.sv
@@ -32,10 +32,10 @@ module detect_4_bit_sequence_using_fsm
     // verilator lint_off CASEINCOMPLETE
 
     case (state)
-      IDLE: if (  a) new_state = F1;
-      F1:   if (~ a) new_state = F0;
-      F0:   if (  a) new_state = S1;
-            else     new_state = IDLE;
+      IDLE: if (  a) new_state = F1; // 0000 -> 0001
+      F1:   if (~ a) new_state = F0; // 0001 -> 0010
+      F0:   if (  a) new_state = S1; // 0010 -> 0101
+            else     new_state = IDLE; // 0010 -> 0100 -> IDLE
       S1:   if (~ a) new_state = S0;
             else     new_state = F1;
       S0:   if (  a) new_state = S1;
@@ -74,6 +74,68 @@ module detect_6_bit_sequence_using_fsm
   // Implement a module that detects the "110011" input sequence
   //
   // Hint: See Lecture 3 for details
+
+
+
+
+  // Studend note:
+  // The task is "Implement a module that detects the "110011" input sequence"
+  // And there do not specified will current sequence be part of new sequence or not
+  // I decided that it should be, beccause thats it's how example implemented
+  // but in my opinion task description have to be more detailed, because othe implementation is accetable
+
+
+
+
+
+  enum logic[2:0]
+  {
+     IDLE       = 3'd1,
+     S_xxxxx1   = 3'd2,
+     S_xxxx11   = 3'd3,
+     S_xxx110   = 3'd4,
+     S_xx1100   = 3'd5,
+     S_x11001   = 3'd6,
+     S_110011   = 3'd7
+  }
+  state, new_state;
+
+  // State transition logic
+  always_comb
+  begin
+    new_state = state;
+
+    // This lint warning is bogus because we assign the default value above
+    // verilator lint_off CASEINCOMPLETE
+
+    case (state)
+      IDLE:       if (  a) new_state = S_xxxxx1;
+      S_xxxxx1:   if (  a) new_state = S_xxxx11;
+                  else     new_state = IDLE;
+      S_xxxx11:   if ( ~a) new_state = S_xxx110; 
+      S_xxx110:   if ( ~a) new_state = S_xx1100;
+                  else     new_state = S_xxxxx1;   
+      S_xx1100:   if (  a) new_state = S_x11001;
+                  else     new_state = IDLE;  
+      S_x11001:   if (  a) new_state = S_110011;
+                  else     new_state = IDLE; 
+      S_110011:   if (  a) new_state = S_xxxx11; 
+                  else new_state = S_xxx110; 
+    endcase
+
+    // verilator lint_on CASEINCOMPLETE
+
+  end
+
+  // Output logic (depends only on the current state)
+  assign detected = (state == S_110011);
+
+  // State update
+  always_ff @ (posedge clk)
+    if (rst)
+      state <= IDLE;
+    else
+      state <= new_state;
 
 
 endmodule
--- 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
+++ 03_finite_state_machines/03_07_sort_floats_using_fsm/03_07_sort_floats_using_fsm.sv
@@ -31,11 +31,108 @@ module sort_floats_using_fsm (
     //
     // Notes:
     // res0 must be less or equal to the res1
-    // res1 must be less or equal to the res1
+    // res1 must be less or equal to the res2
     //
     // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
     // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
 
 
+    enum logic [2:0]
+    {
+        IDLE      = 3'd0,
+        WAIT_A    = 3'd1,
+        WAIT_B    = 3'd2,
+        WAIT_C    = 3'd3
+    } state, new_state;
 
+    // State transition logic
+    always_comb
+    begin
+        new_state = state;
+
+        case (state)
+            IDLE:   if (valid_in) new_state = WAIT_A;
+            WAIT_A: new_state = WAIT_B;
+            WAIT_B: new_state = WAIT_C;
+            WAIT_C: new_state = IDLE;
+        endcase
+    end
+
+    // State update
+    always_ff @ (posedge clk)
+        if (rst)
+            state <= IDLE;
+        else
+            state <= new_state;
+
+    // Output logic (depends only on the current state)
+    assign busy = (state != IDLE);
+    assign valid_out = (state == WAIT_C);
+
+    // order position
+    // order[0] is 0 leq 2
+    // order[1] is 1 leq 2
+    // order[2] is 0 leq 1
+    enum logic [2:0]
+    {
+        st_a        = 3'b000, // 0 >  1 1 >  2 0 >  2: {2 < 1 < 0}
+        st_b        = 3'b001, // 0 >  1 1 >  2 0 <= 2: {error}
+        st_c        = 3'b010, // 0 >  1 1 <= 2 0 >  2: {1 < 2 < 0}
+        st_d        = 3'b011, // 0 >  1 1 <= 2 0 <= 2: {1 < 0 < 2}
+        st_e        = 3'b100, // 0 <= 1 1 >  2 0 >  2: {2 < 0 < 1}
+        st_f        = 3'b101, // 0 <= 1 1 >  2 0 <= 2: {0 < 2 < 1}
+        st_g        = 3'b110, // 0 <= 1 1 <= 2 0 >  2: {error}
+        st_h        = 3'b111  // 0 <= 1 1 <= 2 0 <= 2: {0 < 1 < 2}
+    } order;
+
+    logic [2:0] error;
+    logic [1:0] compare_error;
+
+    always_ff @ (clk) 
+        if (rst) begin
+            order <= 0;
+            error <= 0;
+        end
+        else if (state == WAIT_A) begin
+            f_le_a      <= unsorted[0];
+            f_le_b      <= unsorted[2];
+        end
+        else if (state == WAIT_B) begin
+            f_le_a      <= unsorted[1];
+            f_le_b      <= unsorted[2];
+        end
+        else if (state == WAIT_C) begin
+            f_le_a      <= unsorted[0];
+            f_le_b      <= unsorted[1];
+        end
+
+    always_comb begin
+        compare_error = 2'b00;
+
+        if (state == WAIT_A) begin
+            order[0]    = f_le_res;
+            error[0]    = f_le_err;
+        end
+        else if (state == WAIT_B) begin
+            order[1]    = f_le_res;
+            error[1]    = f_le_err;
+        end
+        else if (state == WAIT_C) begin
+            order[2]    = f_le_res;
+            error[2]    = f_le_err;
+
+            case (order)
+                st_a: sorted = { unsorted [2], unsorted [1], unsorted [0] };
+                st_b: compare_error[0] = 1'b1; 
+                st_c: sorted = { unsorted [1], unsorted [2], unsorted [0] };
+                st_d: sorted = { unsorted [1], unsorted [0], unsorted [2] };
+                st_e: sorted = { unsorted [2], unsorted [0], unsorted [1] };
+                st_f: sorted = { unsorted [0], unsorted [2], unsorted [1] };
+                st_g: compare_error[1] = 1'b1;
+                st_h: sorted = { unsorted [0], unsorted [1], unsorted [2] };
+            endcase
+        end
+
+        err = error[0] | error[1] | error[2] | compare_error[0] | compare_error[1];
+    end
 endmodule
